af2bedd0eb763a88c0b017773f76bab6e4a7eaba
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-22 13:00:06 -0600

    Initial project commit of Spoke.io

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Spoker is live! Running on port: ${port}` );
+});
+
