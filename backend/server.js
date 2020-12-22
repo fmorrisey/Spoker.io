4a5e3efbbdc801a8bf3d9c907a97972f12d735a1
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-22 13:29:20 -0600

    Connected mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,34 @@
+// Package dependencies
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const connection = mongoose.connection;
 
 app.use(cors());
 app.use(express.json());
 
+
+//===============================
+// connection string parser for MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true, 
+    useCreateIndex: true
+});
+
+
+connection.once('open', () =>{
+    console.log('MongoDB database connection established successfully')
+})
+
+//================================
+
+// connection port messages for successful launch!
 app.listen(port, () => {
     console.log(`Spoker is live! Running on port: ${port}` );
 });
