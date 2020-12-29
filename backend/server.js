079e8f899d35d335ca7236ca292f5dd132a5c27d
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-29 09:47:09 -0600

    added degguing to the server js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const debug = require('debug');
 
 const app = express();
 
@@ -33,11 +34,20 @@ const updateUser = require('./auth/routes/updateUser');
 // connection string parser for MongoDB
 // connection constructor 
 const uri = process.env.ATLAS_URI;
+const DEBUG = debug("dev");
 mongoose.connect(uri, {
     useNewUrlParser: true, 
     useCreateIndex: true,
+    useFindAndModify: false,
     useUnifiedTopology: true //added 12.22 to use newest
-});
+})
+    .then(() => {
+        DEBUG("MongoDB is connected");
+    })
+    .catch((err) => {
+        DEBUG("MongoDB connection unsuccessful");
+        DEBUG(err);
+    });
 const connection = mongoose.connection;
 
 connection.once('open', () =>{
