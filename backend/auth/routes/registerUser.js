2dc587d37fdf1d6e4ae1b48d338dd3a3a4580ee9
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-28 19:33:24 -0600

    passportjs ver1 complete

---
+++ backend/auth/routes/registerUser.js
@@ -0,0 +1,7 @@
+"use strict";
+const User = require('../models/user.model');
+const passport = require('passport');
+
+module.exports = app => {
+    
+}
--- backend/mongoose.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-import MongoDB from 'MongoDB';
-import UserModel from './auth/models/user'
