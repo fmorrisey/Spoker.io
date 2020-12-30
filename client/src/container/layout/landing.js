aad503d2438f0555b5e8057f872dd1bfcce36a34
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-30 13:07:10 -0600

    created buttons and filler content for reg login

--- client/src/container/layout/landing.js
+++ client/src/container/layout/landing.js
@@ -1,11 +1,21 @@
 
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+//============Route: /home===============
 export default class Landing extends Component {
   render() {
     return (
-      <div>
-        <p>You are on the Landing Page Component!</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 center-align">
+            <div className="col-6">
+              <Link to="/register" className="btn btn-primary waves-effect">Register</Link>
+            </div>
+            <div className="col-6">
+              <Link to="/login" className="btn btn-primary waves-effect">Log In</Link>
+            </div>
+          </div> 
+        </div>
       </div>
     )
   }
