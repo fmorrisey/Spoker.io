9cae7866a1efa208161082ee8e52d29cc5b659f9
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-31 14:45:50 -0600

    appjs now only renders routers

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -2,18 +2,11 @@
 import React from 'react';
 import Routes from '../routes';
 
-//========= REDUX =============
-import { Provider } from 'react-redux';
-import store from '../store';
-
-
 function App() {
   return (
-    <Provider store={store}>
     <div>
       <Routes />
     </div>
-    </Provider>
   );
 }
 
