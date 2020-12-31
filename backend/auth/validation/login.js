a8dba2e0e95a66157d1894b989b6575bb2426fa5
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-31 14:42:00 -0600

    login changed back to email login

--- backend/auth/validation/login.js
+++ backend/auth/validation/login.js
@@ -4,17 +4,16 @@ const isEmpty = require("is-empty");
 module.exports = function validateLoginInput(data) {
   let errors = {};
 
-  // Converts empty fields
-  data.username = !isEmpty(data.username) ? data.username : "";
-  password = !isEmpty(data.password) ? data.password : "";
+  // Convert empty fields to an empty string so we can use validator functions
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
 
-   // Email checks
-   if (Validator.isEmpty(data.email)) {
+  // Email checks
+  if (Validator.isEmpty(data.email)) {
     errors.email = "Email field is required";
   } else if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
   }
-
   // Password checks
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
@@ -24,5 +23,4 @@ module.exports = function validateLoginInput(data) {
     errors,
     isValid: isEmpty(errors)
   };
-
 };
