2307de3ddb1ae29902a2981f4a2f0c16451165a8
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-31 12:54:38 -0600

    redux linked to the project

--- client/src/components/private-route.js
+++ client/src/components/private-route.js
@@ -1,4 +1,3 @@
 import React, { Component } from "react";
-import {Route, Redirect } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import { Connect } from "react-redux";
-
