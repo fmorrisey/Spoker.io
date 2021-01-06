3e5d05c799d0407745a55109cd61a160582a0cd5
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-01-06 12:33:05 -0600

    order router can push append products to the array

--- backend/inventory/routes/order.js
+++ backend/inventory/routes/order.js
@@ -6,24 +6,26 @@ const { auth } = require('../../auth/middleware/auth');
 const Str = require('@supercharge/strings')
 
 const random = Str.random() 
-
+//=========GET============
 router.route('/').get((req, res) => {
     Order.find()
-           .then(orders => res.json(orders))
-           .catch(err => res.status(400).json('Error: ' + err));
+    .then(orders => res.json(orders))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
+//=========GET BY ID============
 router.route('/:id').get([auth], (req, res) => {
     Order.findById(req.params.id)
-         .then(order => res.json(order))
-         .catch(err => res.status(400).json('Error: ' + err))
+    .then(order => res.json(order))
+    .catch(err => res.status(400).json('Error: ' + err))
 });
 
 function findAddressById(addressId) {
     Address.findById(addressId).populate('user', '_id')
-           .then(address => {return address})
+    .then(address => {return address})
 };
 
+//=========CREATE/ADD ORDER============
 router.post('/add', [auth], (req, res) => {
     console.log("req: ", req.body)
     const user = req.user.id;
@@ -34,10 +36,11 @@ router.post('/add', [auth], (req, res) => {
     const address = findAddressById(req.user.address);
     
     const newOrder = new Order({
+        user,
         trackingNumber,
+        prodId,
         orderStatus,
         pickUpStatus,        
-        user,
         address,
     });
 
@@ -46,6 +49,32 @@ router.post('/add', [auth], (req, res) => {
     .then(() => res.json('Order added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 
+
+});
+
+//============DELETE======
+router.route('/:id').delete((req, res) => {
+    Order.findByIdAndDelete(req.params.id)
+           .then(() => res.json('Product Deleted'))
+           .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//============UPDATE======
+router.route('/update/:id').post((req, res) => {
+    Order.findById(req.params.id)
+           .then(order => {
+            if (req.body.prodId != order.prodId) {
+                order.prodId.push(req.body.prodId);
+            }
+            order.trackingNumber = req.body.trackingNumber;
+            order.orderStatus = req.body.orderStatus;
+            order.pickUpStatus = req.body.pickUpStatus; 
+            order.address = req.body.address;
+    
+            order.save()
+            .then(() => res.json(' Updated!' + order))
+            .catch(err => res.status(400).json('Error: ' + err));
+           })
 
 });
 
