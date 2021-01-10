09fe22b9fe7c5b2776860760b60e48ce81c173b5
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-01-10 09:42:11 -0600

    chartis now added to the index and inital sales data is inlcuded

--- client/src/container/manager/sales.js
+++ client/src/container/manager/sales.js
@@ -5,42 +5,65 @@ import axios from "axios";
 class SalesManager extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       role: " ",
       sales: [],
-     };
-
+      inventory: [],
+    };
   }
-  
+
   componentDidMount() {
-    this.setState({ role: this.props.auth.user.role.toLowerCase() });
     axios
-      .get('http://localhost:5000/orders/getsold')
+      .get("http://localhost:5000/orders/getSales")
       .then((response) => {
-        if (response.data.length > 0) {
-          this.setState({
-            sales: response.data.map(
-              (sale) => sale.sale
-            ),
-            sale: response.data[0].sale
-          })
-        }
+        this.setState({ sales: response.data });
       })
-
-      var x;
-      for (let i = 0; i < this.state.sales.length; i++) {
-        x += this.state.sales[i].price;
-        console.log(x)
-      }
-      console.log(x);
+      .catch((error) => {
+        console.log(error);
+      });
+    axios
+      .get("http://localhost:5000/orders/margins")
+      .then((response) => {
+        this.setState({ inventory: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
+
   render() {
-   
+    console.log(this.state.sales);
 
     return (
-      <div>
-        <p>You are on the Sales component!</p>
-        <p>{this.state.role}</p>
+      <div className="container">
+        <h2 className="">Revenue</h2>
+        <div className="row">
+          <div className="col-md-6">
+            <div>
+              <h3>Sales Data:</h3>
+              <p>Retail ${this.state.sales.retailSales}</p>
+              <p>Cost ${this.state.sales.msrpCost}</p>
+              <p>Profits ${this.state.sales.profit}</p>
+              <p>Margin %{this.state.sales.percentage}</p>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div>
+              <h3>Inventory:</h3>
+              <p>Retail projection ${this.state.inventory.retailSales}</p>
+              <p>Inventory value at cost ${this.state.inventory.msrpCost}</p>
+              <p>Profit potential ${this.state.inventory.profit}</p>
+              <p>Margin %{this.state.inventory.percentage}</p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="">
+              <h2>CHARTS</h2>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
