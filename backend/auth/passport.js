48421d15559e0d9ea8917be613559a2700b52949
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-02-22 09:20:43 -0600

    left comment about passport

--- backend/auth/passport.js
+++ backend/auth/passport.js
@@ -1,3 +1,4 @@
+// Is not currently in use but left for future implementations
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
